62fd2892bd30346004f18e11025f357e8a807bd7
AzizbekAbrorovich <azizbeyabrorovich@gmail.com>
2024-12-07 13:49:56 +0500

    added head nurse history and profile

--- src/renderer/pages/Profile.jsx
+++ src/renderer/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserProfile from '../components/profile/UserProfile';
+import HeadNurseProfile from '../components/profile/HeadNurseProfile';
 
 const Profile = () => {
   
@@ -8,10 +9,10 @@ const Profile = () => {
     switch (role) {
       case "user":
         return <UserProfile />;
-    //   case "headNurse":
-    //     return <NurseProfile />;
-    //   case "doctor":
-    //     return <DoctorProfile />;
+       case "headNurse":
+        return <HeadNurseProfile />;
+       case "doctor":
+         return <DoctorProfile />;
       default:
         console.log("Role not chosen");
         return null;
