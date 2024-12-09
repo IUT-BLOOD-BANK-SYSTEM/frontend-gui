de278af9b76738ab008bc05feb828d8592efc7d1
Ibrohim229 <botirjonov22@gmail.com>
2024-12-09 23:56:34 +0500

    Added modals to Bloodbank page

--- src/renderer/components/reusable/SubmitButton.jsx
+++ src/renderer/components/reusable/SubmitButton.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-const SubmitButton = ({ variant = "solid", text="Submit", children, onClick }) => {
-  const baseStyles = "w-full px-4 py-2 mt-7 rounded-md font-semibold transition";
+const SubmitButton = ({
+  variant = "solid",
+  text = "Submit",
+  children,
+  onClick,
+}) => {
+  const baseStyles =
+    "w-full px-4 py-2 mt-7 rounded-md font-semibold transition";
   const variants = {
     solid: "bg-red-500 text-white hover:bg-red-700",
     outline: "bg-transparent border border-white text-white hover:opacity-80",
   };
   return (
-    <button
-      className={`${baseStyles} ${variants[variant]}`}
-      onClick={onClick}
-    >
+    <button className={`${baseStyles} ${variants[variant]}`} onClick={onClick}>
       {text}
       {children}
     </button>
--- src/renderer/pages/Bloodbank.jsx
+++ src/renderer/pages/Bloodbank.jsx
@@ -2,6 +2,20 @@ import { CirclePlus, Trash } from "lucide-react";
 import { Button } from "../components/ui/button";
 import BloodInfoCard from "../components/reusable/BloodInfoCard";
 import React from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+import FormField from "../components/reusable/FormField";
+
+import { bloodTypes } from "../lib/utils";
+import SubmitButton from "../components/reusable/SubmitButton";
 
 const bloodData = [
   { bloodType: "A+", bloodAmount: "0 L" },
@@ -26,12 +40,121 @@ const BloodBank = () => {
         ))}
       </div>
       <div className="flex items-center justify-end gap-5">
-        <Button className="bg-success flex items-center w-[149px] h-[46px] gap-x-2 text-lg font-semibold hover:bg-emerald-600">
-          <CirclePlus /> Add
-        </Button>
-        <Button className="bg-secondary flex items-center w-[149px] h-[46px] gap-x-2 text-lg font-semibold hover:bg-red-700">
-          <Trash /> Remove
-        </Button>
+        <Dialog>
+          <DialogTrigger>
+            <Button className="bg-success flex items-center w-[149px] h-[46px] gap-x-2 text-lg font-semibold hover:bg-emerald-600">
+              <CirclePlus /> Add
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <div className="flex flex-col ">
+              <DialogHeader className={`text-black`}>
+                <DialogTitle>Add blood</DialogTitle>
+              </DialogHeader>
+
+              <div className="grid grid-cols-2 gap-2 mt-7">
+                <FormField
+                  name="A+"
+                  label="Blood type"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="select"
+                  options={bloodTypes}
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+
+                <FormField
+                  name="amount"
+                  label="Amount"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="text"
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+
+                <FormField
+                  name="name"
+                  label="Donor's name"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="text"
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+
+                <FormField
+                  name="id"
+                  label="Passport ID"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="text"
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <SubmitButton text="Add to blood bank" />
+                <DialogFooter className="sm:justify-start">
+                  <DialogClose asChild>
+                    <Button className="bg-transparent text-black hover:bg-gray-100 border border-black text-[17px] w-full">
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog>
+          <DialogTrigger>
+            <Button className="bg-secondary flex items-center w-[149px] h-[46px] gap-x-2 text-lg font-semibold hover:bg-red-700">
+              <Trash /> Remove
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <div className="flex flex-col ">
+              <DialogHeader className={`text-black`}>
+                <DialogTitle>Remove blood</DialogTitle>
+              </DialogHeader>
+
+              <div className="grid grid-cols-2 gap-2 mt-7">
+                <FormField
+                  name="A+"
+                  label="Blood type"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="select"
+                  options={bloodTypes}
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+
+                <FormField
+                  name="amount"
+                  label="Amount"
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  type="text"
+                  required={true}
+                  borderColor="border-[#B9B9B9]"
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <SubmitButton text="Remove from blood bank" />
+                <DialogFooter className="sm:justify-start">
+                  <DialogClose asChild>
+                    <Button className="bg-transparent text-black hover:bg-gray-100 border border-black text-[17px] w-full">
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </section>
   );
